7a3c067abf6d79d2cf58acdc1319893f5efac2c4
Ambert Jean-Christophe <jc.ambert@free.fr>
2016-03-29 18:16:41 +0200

    First Commit

---
+++ HomeThings/HomeThings/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HomeThings.Startup))]
+
+namespace HomeThings
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Pour plus d'informations sur la façon de configurer votre application, consultez http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
