edc4d51a1b0649e06c64bdf8fe2c985f94bd5e04
Ambert Jean-Christophe <jc.ambert@free.fr>
2016-04-07 18:20:17 +0200

    First Commit

--- HomeThings.Diagram/src/app/diagram/diagram.js
+++ HomeThings.Diagram/src/app/diagram/diagram.js
@@ -38,21 +38,30 @@
           replace:true,
           transclude:true,
           template:'<div class="paper" ng-transclude></div>',
-          controller:['$scope','$log',function($scope,$log){
-              $scope.selected=undefined;
+          controller:['$rootScope', '$scope','$log',function($rootScope,$scope,$log){
+              $scope.selected = undefined;
+              $scope.transform = undefined;
               this.selected = function(element){
-                  $log.log('element selected');
-                  $log.log(element);
                   $scope.selected = element;
                   $scope.$apply();
               }
-              
+              this.transform = function(transform){
+                  $scope.transform = transform;
+                  $rootScope.$emit('transformChanged',{"val":transform});
+                  $scope.$apply();
+              }
               $scope.$watch('selected',function(newVal,oldVal){
                   if(newVal!=oldVal){
-                        $scope.$broadcast('selectedChanged',{"val":newVal});
-                        $log.log('broadcast');
+                        $rootScope.$emit('selectedChanged',{"val":newVal});
                   }
-              })
+              });
+              
+              $scope.$watch('transform',function(newVal,oldVal){
+                  $log.log('transform');
+                  if(newVal!=oldVal){
+                        $rootScope.$emit('transformChanged',{"val":newVal});
+                  }
+              });
           }],
           link:function($scope,$element,$attrs){
               
@@ -77,29 +86,37 @@
       };
   }]);
   
-  diagram.directive('transformZone',['util','$log','$',function($util,$log,$){
+  diagram.directive('transformZone',['util','$log','$','$rootScope',function($util,$log,$,$rootScope){
       return {
         restrict:'E',
         replace:true,
         templateUrl:'app/diagram/transformzone.directive.html',
         require:'^paper',
-        controller:['$scope',function($scope){
-            
+        controller:['$rootScope', '$scope',function($rootScope,$scope){
+           
+           
         }],
         link:function($scope,$element,$attrs,parentCtrl){
             $scope.selected = undefined;
+            $scope.transform = undefined;
             $scope.$parent.$watch('selected',function(){
-                $log.log('selected has changed');
-                $log.log($scope.$parent.selected);
+               // $log.log('selected has changed');
+                //$log.log($scope.$parent.selected);
                 $scope.selected=$scope.$parent.selected;
                 
-                var transformAttr = $($scope.selected).attr('transform') || '';
-                var transform= $util.parseTransformString( transformAttr);
+              
                 
-                
-                $log.log(transform);
                 
             });
+            
+            $rootScope.$on('transformChanged',function(){
+                $scope.transform = $scope.$parent.transform;
+                var transformAttr = $($scope.selected).attr('transform') || '';
+                var transform= $util.parseTransformString( transformAttr);
+                $log.log(transform);
+                $element.css('top',transform.translate.ty);
+                $element.css('left',transform.translate.tx);
+            })
             
             $scope.hasSelected = function(){
                 return $scope.selected!=undefined;
@@ -116,17 +133,19 @@
           templateNamespace:'svg',
           templateUrl: 'app/diagram/simpleshape.html',
           link:function(scope,element,attrs,parentCtrl){
-              $log.log(attrs);
+             /* $log.log(attrs);
               if(attrs['draggable'])
                 element.on('mousedown',function(event){
+                    $log.log('shape selected');
                     parentCtrl.selected(element);
                 });
               var el = (attrs.targetId != null) ? angular.element(document.getElementById(attrs.targetId)) : element;
+              */
           }
       };
   }]);
   
-  diagram.directive('draggable',['util','$document','$log',function($util,$document,$log){
+  diagram.directive('draggable',['util','$document','$','$log',function($util,$document,$,$log){
       return{
           restrict:'A',
           require:'^paper',
@@ -135,33 +154,33 @@
                 var transformAttr = $attrs['transform'] || '';
                 var transform= $util.parseTransformString( transformAttr);
                 
-                var startX = 0, startY = 0, x = transform.translate.tx, y = transform.translate.ty;
+                var startX = 0, startY = 0;
                 
                 $element.on('mousedown', function(event) {
                     // Prevent default dragging of selected content
                     event.preventDefault();
-                    startX = event.pageX - x;
-                    startY = event.pageY - y;
+                    startX = event.pageX - transform.translate.tx;
+                    startY = event.pageY - transform.translate.ty;
                     $document.on('mousemove', mousemove);
                     $document.on('mouseup', mouseup);
                     
                     parentCtrl.selected($element);
+                     parentCtrl.transform(transform);
                 });
                 
                 
                 function mousemove(event) {
-                    y = event.pageY - startY;
-                    x = event.pageX - startX;
-                    $element.css({
-                        transform: 'translate('+x+'px,'+y+'px)',
-                        WebkitTransform: 'translate('+x+'px,'+y+'px)'
-                    });
-                    //console.log($element.css('transform'));
+                    transform.translate.ty = event.pageY - startY;
+                    transform.translate.tx = event.pageX - startX;
+                    
+                    $($element).attr('transform',transform.toString());
+                    parentCtrl.transform(transform);
                 }
 
                 function mouseup() {
                     $document.off('mousemove', mousemove);
                     $document.off('mouseup', mouseup);
+                    parentCtrl.selected(undefined);
                 }
           }
       };
@@ -182,7 +201,7 @@
       }
   }]);
   
-  diagram.service('util',[function () {
+  diagram.service('util',['$log',function ($log) {
       this.uuid = function() {
 
             // credit: http://stackoverflow.com/posts/2117523/revisions
@@ -194,33 +213,60 @@
             });
         };
         
-        
+       
        this.parseTransformString =function (transform) {
             var translate,
                 rotate,
                 scale;
 
+            var t={},r={},s={};    
             if (transform) {
-
                 var separator = /[ ,]+/;
 
-                var translateMatch = transform.match(/translate\((.*)\)/);
-                if (translateMatch) {
-                    translate = translateMatch[1].split(separator);
-                }
-                var rotateMatch = transform.match(/rotate\((.*)\)/);
-                if (rotateMatch) {
-                    rotate = rotateMatch[1].split(separator);
-                }
-                var scaleMatch = transform.match(/scale\((.*)\)/);
-                if (scaleMatch) {
-                    scale = scaleMatch[1].split(separator);
-                }
+                var transforms=transform.split(' ');
+                
+                transforms.forEach(function(element) {
+                    //$log.log(element);
+                    var translateMatch = element.match(/translate\((.*)\)/);
+                    if (translateMatch) {
+                        translate = translateMatch[1].split(separator);
+                    }
+                    var rotateMatch = element.match(/rotate\((.*)\)/);
+                    if (rotateMatch) {
+                        rotate = rotateMatch[1].split(separator);
+                        //$log.log(rotate);
+                        //$log.log(parseInt(rotate[1], 10)); 
+                    }
+                    var scaleMatch = element.match(/scale\((.*)\)/);
+                    if (scaleMatch) {
+                        scale = scaleMatch[1].split(separator);
+                    }
+                }, this);
+               
+                
+               
+                 
             }
 
             var sx = (scale && scale[0]) ? parseFloat(scale[0]) : 1;
-
-            return {
+            
+            t.tx=(translate && translate[0]) ? parseInt(translate[0], 10) : 0;
+            t.ty=(translate && translate[1]) ? parseInt(translate[1], 10) : 0
+            
+            r.cx=(rotate && rotate[1]) ? parseInt(rotate[1], 10) : 0;
+            r.cy=(rotate && rotate[2]) ? parseInt(rotate[2], 10) : 0;
+            r.angle=(rotate && rotate[0]) ? parseInt(rotate[0], 10) : 0;
+            
+            s.sx=sx;
+            s.sy= (scale && scale[1]) ? parseFloat(scale[1]) : sx;
+         
+             
+            var toString = function(){
+                var result='translate('+t.tx+','+t.ty+') rotate('+r.angle+','+r.cx+','+r.cy+') scale('+s.sx+','+s.sy+')' ;
+                
+                return result;
+            }
+            /*return {
                 translate: {
                     tx: (translate && translate[0]) ? parseInt(translate[0], 10) : 0,
                     ty: (translate && translate[1]) ? parseInt(translate[1], 10) : 0
@@ -233,8 +279,17 @@
                 scale: {
                     sx: sx,
                     sy: (scale && scale[1]) ? parseFloat(scale[1]) : sx
-                }
+                },
+                
+            };*/
+            
+            return{
+                translate:t,
+                rotate:r,
+                scale:s,
+                toString:toString
             };
         }
+        
   }]);
  })(angular,window);
